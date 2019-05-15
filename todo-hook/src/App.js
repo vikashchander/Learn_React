2b2ca4c6b5861f5af56520236cc96cbaf2e2bc0c
vikash chander <vikashchander78384@gmail.com>
2019-05-15 23:12:28 +0530

    todo app through hooks

---
+++ todo-hook/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from './todo/todo';
+
+
+function App() {
+  return (
+    <div >
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-hook/src/todo/todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import  Grid from '@material-ui/core/Grid'; 
+function todo(){
+    return (
+        <div>
+        <h2>hello there !!!</h2>
+        </div>
+    )
+}
+
+export default todo;
